58162bf13fc78e802b06a13bee4f0c006961a23f
desoleh <Muchamad.soleh@gmail.com>
2021-12-28 18:39:12 +0700

    wire ui

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', '| Frontliner') }}</title>
+
+    <!-- Favicons -->
+    <link rel="icon" sizes="180x180" href="{{ asset('images/favicon/favicon.ico') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicon/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon/favicon-16x16.png') }}">
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
@@ -37,5 +42,7 @@
         </main>
     </div>
 </div>
+
+    @wireUiScripts
 </body>
 </html>
